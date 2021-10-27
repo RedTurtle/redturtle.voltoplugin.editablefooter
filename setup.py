5aa450fa775ce5dd47fbabf40fb73b0d0d4cf1b4
nzambello <nzambello@protonmail.com>
2021-10-27 12:20:14 +0200

    fix: python_requires

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires="==2.7, >=3.6",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
         "setuptools",
         "plone.api> =1.8.4",
